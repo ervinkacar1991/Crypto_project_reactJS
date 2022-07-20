6c9b2af108f40720e2112154cb3aed6d95ec09bd
ervinkacar1991 <kacarervin@gmail.com>
2022-07-20 11:08:52 +0200

    Working on CoinPage.jsx page

--- src/routes/CoinPage.jsx
+++ src/routes/CoinPage.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Sparklines, SparklinesLine } from "react-sparklines";
+import { FaTwitter, FaFacebook, FaReddit, FaGithub } from "react-icons/fa";
 import axios from "axios";
+import DOMPurify from "dompurify";
 
 const CoinPage = () => {
   const [coin, setCoin] = useState({});
@@ -34,8 +36,120 @@ const CoinPage = () => {
               <SparklinesLine color="teal" />
             </Sparklines>
           </div>
-          /*MARKET CAP NEED TO FINISH */
+
+          <div>
+            <div>
+              <p>Market Cap</p>
+              {coin.market_data?.market_cap ? (
+                <p>${coin.market_data.market_cap.usd.toLocaleString()}</p>
+              ) : null}
+            </div>
+            <div>
+              <p>Volume (24h)</p>
+              {coin.market_data?.market_cap ? (
+                <p>${coin.market_data.total_volume.usd.toLocaleString()}</p>
+              ) : null}
+            </div>
+          </div>
+          <div>
+            <div>
+              <p>24h High</p>
+              {coin.market_data?.high_24h ? (
+                <p>${coin.market_data.high_24h.usd.toLocaleString()}</p>
+              ) : null}
+            </div>
+            <div>
+              <p>24h Low</p>
+              {coin.market_data?.low_24h ? (
+                <p>${coin.market_data.low_24h.usd.toLocaleString()}</p>
+              ) : null}
+            </div>
+          </div>
         </div>
+        <div>
+          <p>Market Stats</p>
+          <div>
+            <div>
+              <p>Market Rank</p>
+              {coin.market_cap_rank}
+            </div>
+            <div>
+              <p>Hashing Algorithm</p>
+              {coin.hashing_algorithm ? <p>{coin.hashing_algorithm}</p> : null}
+            </div>
+            <div>
+              <p>Trust Score</p>
+              {coin.tickers ? <p>{coin.liquidity_score.toFixed(2)}</p> : null}
+            </div>
+          </div>
+          <div>
+            <div>
+              <p>Price Change (24h)</p>
+              {coin.market_data ? (
+                <p>
+                  {coin.market_data.price_change_percentage_24h.toFixed(2)}%
+                </p>
+              ) : null}
+            </div>
+            <div>
+              <p>Price Change (7d)</p>
+              {coin.market_data ? (
+                <p>{coin.market_data.price_change_percentage_7d.toFixed(2)}%</p>
+              ) : null}
+            </div>
+            <div>
+              <p>Price Change (14d)</p>
+              {coin.market_data ? (
+                <p>
+                  {coin.market_data.price_change_percentage_14d.toFixed(2)}%
+                </p>
+              ) : null}
+            </div>
+          </div>
+          <div>
+            <div>
+              <p>Price Change (30d)</p>
+              {coin.market_data ? (
+                <p>
+                  {coin.market_data.price_change_percentage_30d.toFixed(2)}%
+                </p>
+              ) : null}
+            </div>
+            <div>
+              <p>Price Change (60d)</p>
+              {coin.market_data ? (
+                <p>
+                  {coin.market_data.price_change_percentage_60d.toFixed(2)}%
+                </p>
+              ) : null}
+            </div>
+            <div>
+              <p>Price Change (1y)</p>
+              {coin.market_data ? (
+                <p>{coin.market_data.price_change_percentage_1y.toFixed(2)}%</p>
+              ) : null}
+            </div>
+          </div>
+          <div>
+            <FaTwitter />
+            <FaFacebook />
+            <FaReddit />
+            <FaGithub />
+          </div>
+        </div>
+      </div>
+
+      {/**Description fecthing */}
+
+      <div>
+        <p>About {coin.name}</p>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: DOMPurify.sanitize(
+              coin.description ? coin.description.en : ""
+            ),
+          }}
+        ></p>
       </div>
     </div>
   );
