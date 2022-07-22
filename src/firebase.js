1faec87587efa04dc5cce214fee3446e0ade628d
ervinkacar1991 <kacarervin@gmail.com>
2022-07-22 10:30:41 +0200

    Create Context for SignIn and SignUp and Logout

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCDP6po5XZKrEnqE1rWqAoVX3g-Fh8Q8gw",
-  authDomain: "cryptobase-a4843.firebaseapp.com",
-  projectId: "cryptobase-a4843",
-  storageBucket: "cryptobase-a4843.appspot.com",
-  messagingSenderId: "189620927725",
-  appId: "1:189620927725:web:cc12271dc4779105d4e0e4",
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER,
+  appId: REACT_APP_FIREBASE_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
