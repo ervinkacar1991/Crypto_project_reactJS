988542687f763f84da636e1220a15c2d65490114
ervinkacar1991 <kacarervin@gmail.com>
2022-07-22 09:59:52 +0200

    Setting up a firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDP6po5XZKrEnqE1rWqAoVX3g-Fh8Q8gw",
+  authDomain: "cryptobase-a4843.firebaseapp.com",
+  projectId: "cryptobase-a4843",
+  storageBucket: "cryptobase-a4843.appspot.com",
+  messagingSenderId: "189620927725",
+  appId: "1:189620927725:web:cc12271dc4779105d4e0e4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
